f31b20ecad4395aac573f348b1db761190b24ed7
Azeez Muibi <amuibi@softalliance.com>
2025-03-19 11:25:28 +0100

    Update

--- config/filestorage.php
+++ config/filestorage.php
@@ -52,13 +52,13 @@ return [
     |
     */
     'paths' => [
-        'about_images' => env('FILESTORAGE_ABOUT_IMAGES_PATH', 'about/images'),
-        'blog_banners' => env('FILESTORAGE_BLOG_BANNERS_PATH', 'blog/banners'),
-        'career_banners' => env('FILESTORAGE_CAREER_BANNERS_PATH', 'career/banners'),
-        'blog_images' => env('FILESTORAGE_BLOG_IMAGES_PATH', 'blog/images'),
-        'hero_images' => env('FILESTORAGE_HERO_IMAGES_PATH', 'hero/images'),
-        'user_profiles' => env('FILESTORAGE_USER_PROFILES_PATH', 'user/profiles'),
-        'client_logos' => env('FILESTORAGE_CLIENT_LOGOS_PATH', 'client/logos'),
-        'services' => env('FILESTORAGE_SERVICE_IMAGES_PATH', 'service/images'),
+        'about_images' => config('app.name').'/'.env('FILESTORAGE_ABOUT_IMAGES_PATH', config('app.name').'/about/images'),
+        'blog_banners' => config('app.name').'/'.env('FILESTORAGE_BLOG_BANNERS_PATH', config('app.name').'/blog/banners'),
+        'career_banners' => config('app.name').'/'.env('FILESTORAGE_CAREER_BANNERS_PATH', config('app.name').'/career/banners'),
+        'blog_images' => config('app.name').'/'.env('FILESTORAGE_BLOG_IMAGES_PATH', config('app.name').'/blog/images'),
+        'hero_images' => config('app.name').'/'.env('FILESTORAGE_HERO_IMAGES_PATH', config('app.name').'/hero/images'),
+        'user_profiles' => config('app.name').'/'.env('FILESTORAGE_USER_PROFILES_PATH', config('app.name').'/user/profiles'),
+        'client_logos' => config('app.name').'/'.env('FILESTORAGE_CLIENT_LOGOS_PATH', config('app.name').'/client/logos'),
+        'services' => config('app.name').'/'.env('FILESTORAGE_SERVICE_IMAGES_PATH', config('app.name').'/service/images'),
     ],
 ];
